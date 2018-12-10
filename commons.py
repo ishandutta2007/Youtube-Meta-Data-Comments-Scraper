f23c92ea5c2dbd5917428aeb49d198d45cb83e81
Ishan Dutta <ishandutta2007@gmail.com>
2018-12-11 00:04:05 +0530

    refactoring

---
+++ commons.py
@@ -0,0 +1,176 @@
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+import pafy
+import csv
+from datetime import date, datetime, timedelta
+import sys
+import imp
+# imp.reload(sys)
+# sys.setdefaultencoding('utf8')  
+import os
+import traceback
+
+import google.oauth2.credentials
+
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+import constants
+
+youtube = build(constants.YOUTUBE_API_SERVICE_NAME, constants.YOUTUBE_API_VERSION, developerKey=constants.DEVELOPER_KEY)
+
+def fetch_results(nextPageToken, video_id):
+    results = youtube.commentThreads().list(
+                part="snippet",
+                maxResults=100,
+                videoId=video_id,
+                textFormat="plainText",
+                pageToken=nextPageToken
+            ).execute()
+    totalResults = int(results["pageInfo"]["totalResults"])
+    return totalResults, results
+
+def print_response(response):
+    print(response)
+
+def build_resource(properties):
+    resource = {}
+    for p in properties:
+        # Given a key like "snippet.title", split into "snippet" and "title", where
+        # "snippet" will be an object and "title" will be a property in that object.
+        prop_array = p.split('.')
+        ref = resource
+        for pa in range(0, len(prop_array)):
+            is_array = False
+            key = prop_array[pa]
+
+            # For properties that have array values, convert a name like
+            # "snippet.tags[]" to snippet.tags, and set a flag to handle
+            # the value as an array.
+            if key[-2:] == '[]':
+                key = key[0:len(key)-2:]
+                is_array = True
+
+            if pa == (len(prop_array) - 1):
+                # Leave properties without values out of inserted resource.
+                if properties[p]:
+                    if is_array:
+                        ref[key] = properties[p].split(',')
+                    else:
+                        ref[key] = properties[p]
+            elif key not in ref:
+                # For example, the property is "snippet.title", but the resource does
+                # not yet have a "snippet" object. Create the snippet object here.
+                # Setting "ref = ref[key]" means that in the next time through the
+                # "for pa in range ..." loop, we will be setting a property in the
+                # resource's "snippet" object.
+                ref[key] = {}
+                ref = ref[key]
+            else:
+                # For example, the property is "snippet.description", and the resource
+                # already has a "snippet" object.
+                ref = ref[key]
+    return resource
+
+def remove_empty_kwargs(**kwargs):
+    good_kwargs = {}
+    if kwargs is not None:
+        for key, value in kwargs.items():
+            if value:
+                good_kwargs[key] = value
+    return good_kwargs
+
+def comments_insert(client, properties, **kwargs):
+    resource = build_resource(properties)
+    kwargs = remove_empty_kwargs(**kwargs)
+    response = client.comments().insert(
+        body=resource,
+        **kwargs
+        ).execute()
+    return print_response(response)
+
+def print_comment(len_comments, author, delta, text):
+    if delta.days > 365:
+        print(len_comments  , author, str(int(delta.days/365))+ " years ago", text)
+    elif delta.days > 30:
+        print(len_comments  , author, str(int(delta.days/30))+ " months ago", text)
+    elif delta.days > 7:
+        print(len_comments  , author, str(int(delta.days/7))+ " weeks ago", text)
+    elif delta.days > 0:
+        print(len_comments  , author, str(int(delta.days))+ " days ago", text)
+    elif delta.seconds > 3600:
+        print(len_comments  , author, str(int(delta.seconds/3600))+ " hours ago", text)
+    elif delta.seconds > 60:
+        print(len_comments  , author, str(int(delta.seconds/60))+ " minutes ago", text)
+    else:
+        print(len_comments  , author, str(delta.seconds)+ " seconds ago", text)
+
+def process_and_print_comment(len_comments, item):
+    comment = item["snippet"]["topLevelComment"]
+    comment_id = item['id']
+    author = comment["snippet"]["authorDisplayName"]
+    text = comment["snippet"]["textDisplay"]
+    # print_comment(len_comments, author, datetime.now() - datetime.strptime(comment["snippet"]["publishedAt"],"%Y-%m-%dT%H:%M:%S.000Z"), text)
+    return comment_id, author, text
+
+
+def scrape_metadata(video_id):
+    url = "https://www.youtube.com/watch?v=" + video_id
+    video = pafy.new(url)
+    print('video.title:', video.title)
+    print('video.rating:', video.rating)
+    print('video.viewcount:', video.viewcount)
+    print('video.author:', video.author)
+    print('video.length:', video.length)
+    print('video.duration:', video.duration)
+    print('video.likes:', video.likes)
+    print('video.dislikes:', video.dislikes)
+    print('video.description:', video.description)
+    return video
+
+
+def add_data_to_csv(vID, title, description, author, published, viewcount, duration, likes, dislikes, rating, category, comments):
+    data = [vID, title, description, author, published, viewcount, duration, likes, dislikes, rating, category, comments]
+    with open("scraper.csv", "a") as fp:
+        wr = csv.writer(fp, dialect='excel')
+        wr.writerow(data)
+
+def is_comment_X(text):
+    # TODO
+    if text == 'Bla bla words':
+        return True
+    return False
+
+def scrape_comments(video_id, max_comment_fetch_limit=10000):
+    count = 0
+    comments = []
+    next_page_token = ''
+    while True:
+        try:
+            totalResults, results = fetch_results(next_page_token, video_id)
+
+            count += totalResults
+            for item in results["items"]:
+                comment_id, author, text = process_and_print_comment(len(comments), item)
+                if is_comment_X(text):
+                    comments_insert(client,
+                        {'snippet.parentId': comment_id,
+                        'snippet.textOriginal': "Thank you"},
+                        part='snippet')
+                comments.append([author, text])
+
+            print("fetched %d more comments, total comments count: %d" % (totalResults, count))
+            if count > max_comment_fetch_limit:
+                print('MAx comment fetch limit reached, exiting loop')
+                break
+            next_page_token = results["nextPageToken"]
+        except HttpError as e:
+            print(("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)))
+            traceback.print_exc()
+        except KeyError as e:
+            print(("An KeyError error occurred: %s" % (e)))
+            print('Last page reached, exiting loop')
+            break
+    return comments
--- scrape_data_of_top_10_search_results.py
+++ scrape_data_of_top_10_search_results.py
@@ -1,100 +1,40 @@
 from apiclient.discovery import build
-from apiclient.errors import HttpError
+# from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import pafy
 import csv
 
 import sys
 import imp
-# imp.reload(sys)
-# sys.setdefaultencoding('utf8')  
 
 import constants
-DEVELOPER_KEY = constants.DEVELOPER_KEY
+pafy.set_api_key(constants.DEVELOPER_KEY)
+from commons import fetch_results, scrape_metadata, scrape_comments, add_data_to_csv
 
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-pafy.set_api_key(DEVELOPER_KEY)
+youtube = build(constants.YOUTUBE_API_SERVICE_NAME, constants.YOUTUBE_API_VERSION,developerKey=constants.DEVELOPER_KEY)
 
-def add_data(vID,title,description,author,published,viewcount, duration, likes, dislikes,rating,category,comments):
-    data = [vID,title,description,author,published,viewcount, duration, likes, dislikes,rating,category,comments]
-    with open("scraper.csv", "a") as fp:
-        wr = csv.writer(fp, dialect='excel')
-        wr.writerow(data)
+def get_data(video_id):
+    print("\n\nScraping videoId %s" % video_id)
+    video = scrape_metadata(video_id)
+    comments = scrape_comments(video_id)
+    add_data_to_csv(video_id, video.title, video.description, video.author, video.published, video.viewcount, video.duration, video.likes, video.dislikes, video.rating, video.category, comments)
 
-youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
-
-def get_data(videoId):
-    url = "https://www.youtube.com/watch?v=" + videoId
-    #Request fro Metadata of the Video
-    video = pafy.new(url)
-
-    #Request for Comments
-    results = youtube.commentThreads().list(
-                part="snippet",
-                maxResults=100,
-                videoId=videoId,
-                textFormat="plainText"
-              ).execute()
-    totalResults = 0
-    totalResults = int(results["pageInfo"]["totalResults"])
+if __name__ == '__main__':
+    searchTerm = eval(input("Term you want to Search : \n"))
+    no_of_res = eval(input("Number of videos you would want to scrape: \n"))
+    search_response = youtube.search().list(
+        q=searchTerm,
+        part="id,snippet",
+        maxResults=30
+        ).execute()
     count = 0
-    nextPageToken = ''
-    comments = []
-    further = True
-    first = True
-    while further:
-        halt = False
-        if first == False:
-            print(".")
-            try:
-                results = youtube.commentThreads().list(
-                    part="snippet",
-                    maxResults=100,
-                    videoId=videoId,
-                    textFormat="plainText",
-                    pageToken=nextPageToken
-                ).execute()
-                totalResults = int(results["pageInfo"]["totalResults"])
-            except HttpError as e:
-                print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
-                halt = True
-        if halt == False:
-            count += totalResults
-            for item in results["items"]:
-                  comment = item["snippet"]["topLevelComment"]
-                  author = comment["snippet"]["authorDisplayName"]
-                  text = comment["snippet"]["textDisplay"]
-                  comments.append([author,text])
-            if totalResults < 100:
-                further = False
-                first = False
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            if count < no_of_res:
+                vID = search_result["id"]["videoId"]
+                get_data(vID)
+                count += 1
             else:
-                further = True
-                first = False
-                try:
-                    nextPageToken = results["nextPageToken"]
-                except KeyError as e:
-                    print("An KeyError error occurred: %s" % (e))
-                    further = False
-
-    # Adding the full data to CSV
-    add_data(videoId,video.title,video.description,video.author,video.published,video.viewcount, video.duration, video.likes, video.dislikes,video.rating,video.category,comments)
-
-searchTerm = eval(input("Term you want to Search : \n"))
-search_response = youtube.search().list(
-      q=searchTerm,
-      part="id,snippet",
-      maxResults=30
-    ).execute()
-count = 0
-for search_result in search_response.get("items", []):
-    if search_result["id"]["kind"] == "youtube#video":
-        if count <10:
-            vID = search_result["id"]["videoId"]
-            get_data(vID)
-            count += 1
+                break
         else:
-            break
-    else:
-        continue
+            continue
