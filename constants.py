26cabbe4a24f3283ece5cf6e81fe456492a74143
Ishan Dutta <ishandutta2007@gmail.com>
2018-12-10 21:43:56 +0530

    2to3 and refactoring

---
+++ constants.py
@@ -0,0 +1,15 @@
+import os
+import configparser
+config = configparser.ConfigParser()
+config.read("config.txt")
+
+DEVELOPER_KEY = config.get("configuration", "goog_ishandutta2007_apikey")
+
+GOOG_ID = 'ishandutta2007'
+GOOG_PASS = config.get("configuration", "goog_ishandutta2007_password")
+
+CLIENT_SECRETS_FILE = "client_secret_ishandutta2007.json"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+
